3ed007f7ed687973907e89375d288fff750dd07a
GAKUBA JULES <gakubajules666@gmail.com>
2026-05-20 05:27:34 -0700

    Add LoadingSpinner component

---
+++ src/src/src/src/src/types/src/components/src/components/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-netflix-black z-50">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-netflix-red"></div>
+    </div>
+  )
+}
